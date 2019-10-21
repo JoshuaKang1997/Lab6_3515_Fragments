ecc7db3f779d9ea0904b80ed20b5c92b67147bf4
JoshuaKang1997 <joshkang1997@gmail.com>
2019-10-21 17:06:59 -0400

    implemented fragments ONLY, no ColorAdapter Logic in the code yet

--- app/src/main/java/com/edu/temple/paletteactivity2/MainActivity.java
+++ app/src/main/java/com/edu/temple/paletteactivity2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.edu.temple.paletteactivity2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectFrag(View view) {
+
+        Fragment fr;
+
+        if(view == findViewById(R.id.button)){
+            fr = new PaletteFragment();
+        }
+        else{
+            fr = new CanvasFragment();
+        }
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment, fr);
+        transaction.commit();
+
     }
 }
